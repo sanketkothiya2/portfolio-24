9b15f38ec9794a2f4667e5f58d7cb054c348e997
sanket <sanketkothiya53@gmail.com>
2024-12-01 15:15:03 -0500

    add changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["frontend development", "backend development", "web designing", "data analyst", "full stack development","Dev ops", "web development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
